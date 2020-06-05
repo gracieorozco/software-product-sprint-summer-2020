cfb0c0ee515ddadefcb74aca67ceb0fc9fead44d
gracieorozco <orozco.gracie.m@gmail.com>
2020-06-04 19:27:29 -0700

    Add main text content to HTML files

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -17,7 +17,7 @@
  */
 function addRandomGreeting() {
   const greetings =
-      ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
+      ['I major in computer science.', 'I am learning new langauges.', 'I have traveled to over 30 states.'];
 
   // Pick a random greeting.
   const greeting = greetings[Math.floor(Math.random() * greetings.length)];
