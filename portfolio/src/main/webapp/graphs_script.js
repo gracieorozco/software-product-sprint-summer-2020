f246b830e029636f9ee213735b2aa3055cf1e637
gracieorozco <orozco.gracie.m@gmail.com>
2020-06-28 10:18:46 -0700

    Fix minor details in layout

--- portfolio/src/main/webapp/graphs_script.js
+++ portfolio/src/main/webapp/graphs_script.js
@@ -6,11 +6,11 @@ function drawGraph() {
   const data = new google.visualization.DataTable();
   data.addColumn('string', 'Projects');
   data.addColumn('number', 'Number of Lines Coded (#)');
-        data.addRows([
-          ['Information Kiosk', 5300],
-          ['Data Science Research Project', 2568],
-          ['Research Project', 4100]
-        ]);
+  data.addRows([
+    ['Information Kiosk', 5300],
+    ['Data Science Research Project', 2568],
+    ['Research Project', 4100]
+  ]);
 
   const options = {
     'title': 'Number of Lines Coded by Project',
