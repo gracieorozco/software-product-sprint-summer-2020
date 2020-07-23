303d90fa574795c5cc73f45b3e0ebec7593a364e
gracieorozco <orozco.gracie.m@gmail.com>
2020-07-22 22:00:54 -0700

    Fix loop error and update variables

--- walkthroughs/week-4-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-4-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -20,19 +20,21 @@ import java.util.Collections;
 
 public final class FindMeetingQuery {
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
+    ArrayList<TimeRange> result = new ArrayList<TimeRange>();
     ArrayList<TimeRange> timeRanges = new ArrayList<TimeRange>();
     ArrayList<Integer> startTimes = new ArrayList<Integer>();
     ArrayList<Integer> endTimes = new ArrayList<Integer>();
 
     // If the duration is longer than the entire day, return an empty collection. 
     if (request.getDuration() > TimeRange.WHOLE_DAY.duration()) {
-        return timeRanges;
+        return result;
     }
-
+    
     // If there are no events or attendees in the request, return the whole day. 
     if (events.isEmpty() || request.getAttendees().isEmpty()) {
         timeRanges.add(TimeRange.WHOLE_DAY);
-        return timeRanges;
+        result.add(TimeRange.WHOLE_DAY);
+        return result;
     }
 
     // Loop through the list of events to add the start and end times for an event of the requested attendees. 
@@ -45,54 +47,64 @@ public final class FindMeetingQuery {
         }
     }
 
-    // Sort the start and end times to simplify further processing. 
-    Collections.sort(startTimes);
-    Collections.sort(endTimes);
+    // Check if there are any start and end times to generate meeting times, otherwise, return the whole day.
+    if (startTimes.size() > 0 && endTimes.size() > 0) {
+        // Sort the start and end times to simplify further processing. 
+        Collections.sort(startTimes);
+        Collections.sort(endTimes);
 
-    // If the first start time does not begin with 0, include a time starting from 0 to the first start time. 
-    if (startTimes.get(0) != 0) {
-        timeRanges.add(TimeRange.fromStartEnd(0, startTimes.get(0), false));
-    }
+        // If the first start time does not begin with 0, include a time starting from 0 to the first start time. 
+        if (startTimes.get(0) != 0) {
+            timeRanges.add(TimeRange.fromStartEnd(0, startTimes.get(0), false));
+            result.add(TimeRange.fromStartEnd(0, startTimes.get(0), false));
+        }
 
-    // If the last end time does not end with 1440, include a time ending from 1440 with the last end time. 
-    if (endTimes.get(endTimes.size()-1) != 1440) {
-        timeRanges.add(TimeRange.fromStartEnd(endTimes.get(endTimes.size()-1), 1440, false));
+        // If the last end time does not end with 1440, include a time ending from 1440 with the last end time. 
+        if (endTimes.get(endTimes.size()-1) != TimeRange.WHOLE_DAY.duration()) {
+            timeRanges.add(TimeRange.fromStartEnd(endTimes.get(endTimes.size()-1), 1440, false));
+            result.add(TimeRange.fromStartEnd(endTimes.get(endTimes.size()-1), 1440, false));
+        }
+    } else {
+        result.add(TimeRange.WHOLE_DAY);
+        return result;
     }
 
     // Loop through the end times to combine various availability times with the start times.
     for (int i = 0; i < endTimes.size()-1; i++) {
         if (endTimes.get(i) < startTimes.get(i+1)) {
             timeRanges.add(TimeRange.fromStartEnd(endTimes.get(i), startTimes.get(i+1), false));
+            result.add(TimeRange.fromStartEnd(endTimes.get(i), startTimes.get(i+1), false));
         }
     }
 
     // Loops through the time ranges and removes overlapping times. 
     Collections.sort(timeRanges, TimeRange.ORDER_BY_START);
+    Collections.sort(result, TimeRange.ORDER_BY_START);
     for (int i = 0; i < timeRanges.size()-1; i++) { 
         TimeRange currentRange = timeRanges.get(i);
         TimeRange nextRange = timeRanges.get(i+1);
         if (nextRange.overlaps(currentRange)) {
             if (currentRange.contains(nextRange)) {
-                timeRanges.remove(currentRange);
+                result.remove(currentRange);
             } else {
-                timeRanges.remove(nextRange);
+                result.remove(nextRange);
             }
         }
         if (nextRange.start() == nextRange.end()) {
-            timeRanges.remove(nextRange);
+            result.remove(nextRange);
         }
         if (currentRange.start() == currentRange.end()) {
-            timeRanges.remove(currentRange.duration());
+            result.remove(currentRange.duration());
         }
     }
     // Loops through the time ranges once more to remove any ranges that are less than the requested duration. 
     for (int i = 0; i < timeRanges.size(); i++) {
         TimeRange currentRange = timeRanges.get(i);
         if (currentRange.duration() < request.getDuration()) {
-            timeRanges.remove(currentRange);
+            result.remove(currentRange);
         }
     }
 
-    return timeRanges;
+    return result;
   }
 }
